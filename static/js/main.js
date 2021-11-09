d813fd2eb66c22c27775efad2d306550168863d5
Rian Dias de Oliveira <riandiasdeoliveira2001@gmail.com>
2021-11-09 19:50:54 -0300

    Create new script

---
+++ static/js/main.js
@@ -0,0 +1,33 @@
+const userInfo = {
+    fullName: "",
+    aboutUser: "",
+    personalData: {
+        birthDate: "",
+        age: "",
+        nationality: "",
+        address: "",
+        CEP: "",
+        city: "",
+    },
+    contacts: {
+        emailAddress: "",
+        phoneNumber: "",
+        gitHub: "",
+        certificates: "",
+        portfolio: "",
+        linkedin: ""
+    },
+    objectives: "",
+    languages: {
+
+    },
+    skills: {
+
+    },
+    academicFormation: {
+
+    },
+    professionalSkills: {
+
+    }
+}
