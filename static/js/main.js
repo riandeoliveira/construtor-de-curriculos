065c5c0b625c75d929b5280aa265e761b24e57f9
Rian Dias de Oliveira <riandiasdeoliveira2001@gmail.com>
2021-11-10 11:52:28 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,47 @@
-const userInfo = {
-    fullName: "",
-    aboutUser: "",
-    personalData: {
-        birthDate: "",
-        age: "",
-        nationality: "",
-        address: "",
-        CEP: "",
-        city: "",
-    },
-    contacts: {
-        emailAddress: "",
-        phoneNumber: "",
-        gitHub: "",
-        certificates: "",
-        portfolio: "",
-        linkedin: ""
-    },
-    objectives: "",
-    languages: {
+// const userInfo = {
+//     fullName: "",
+//     aboutUser: "",
+//     personalData: {
+//         birthDate: "",
+//         age: "",
+//         nationality: "",
+//         address: "",
+//         CEP: "",
+//         city: "",
+//     },
+//     contacts: {
+//         emailAddress: "",
+//         phoneNumber: "",
+//         gitHub: "",
+//         certificates: "",
+//         portfolio: "",
+//         linkedin: ""
+//     },
+//     objectives: "",
+//     languages: {
 
-    },
-    skills: {
+//     },
+//     skills: {
 
-    },
-    academicFormation: {
+//     },
+//     academicFormation: {
 
-    },
-    professionalSkills: {
+//     },
+//     professionalSkills: {
 
-    }
-}
+//     }
+// }
+
+// function formatBirthDate() {
+//     if (birthDate.value.length == 2 || birthDate.value.length == 5) {
+//         birthDate.value += "/";
+//     }
+// }
+
+const formatBirthDate = () => birthDate.value.length === 2 || birthDate.value.length === 5 ? birthDate.value += "/" : "";
+
+const formatAge = () => age.value.length === 2 ? age.value += " anos" : "";
+
+const formatCEP = () => cep.value.length === 5 ? cep.value += "-" : "";
+
+const formatPhoneNumber = () => phoneNumber.value.length === 1 ? phoneNumber.value += "()" : "";
