ecea504dea91685e32502994f3ea1f9cbd0dd3fb
Rian Dias de Oliveira <riandiasdeoliveira2001@gmail.com>
2021-11-11 17:31:38 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,3 @@
-// const userInfo = {
-//     fullName: "",
-//     textarea: "",
-//     personalData: {
-//         birthDate: "",
-//         age: "",
-//         nationality: "",
-//         address: "",
-//         CEP: "",
-//         city: "",
-//     },
-//     contacts: {
-//         emailAddress: "",
-//         phoneNumber: "",
-//         gitHub: "",
-//         certificates: "",
-//         portfolio: "",
-//         linkedin: ""
-//     },
-//     objectives: "",
-//     languages: {
-
-//     },
-//     skills: {
-
-//     },
-//     academicFormation: {
-
-//     },
-//     professionalSkills: {
-
-//     }
-// }
-
 // Carrega a página com o cursor no nome presente no currículo.
 const focusName = (name) => name.selectionStart = name.selectionEnd = name.value.length;
 
@@ -45,7 +11,7 @@ function formatTextarea(textarea) {
 const elements = document.getElementsByClassName("edit-lists");
 
 // Impede a quebra de linha em alguns elementos textarea.
-for (let i = 0; i <= elements.length; i++) {
+for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener("keypress", keyEvent => {
         if (keyEvent.which === 13) {
             keyEvent.preventDefault();
@@ -53,33 +19,75 @@ for (let i = 0; i <= elements.length; i++) {
     });
 }
 
-// const infoSkillsArea = {
-//     conclusionYear: "";
-//     websiteLink: "";
-//     workload: "";
-//     local: "";
-//     predictedCompletion: "";
-//     shift: "";
-// }
+// Armazena os códigos HTML correspondentes de cada opção selecionada para criá-los quando necessário.
+const infoSkillsArea = {
+    institutionName: `<textarea rows="1" class="titles-2" spellcheck="false" oninput="formatTextarea(this)">Nome da Instituição</textarea>`,
 
-function checkOptions() {
-    if (concluded.checked && online.checked) {
-        
-    }
+    courseName: `<textarea rows="1" class="titles-3" spellcheck="false" oninput="formatTextarea(this)">Nome do Curso</textarea>`,
 
-    if (concluded.checked && presential.checked) {
-        alert("B")
-    }
+    conclusionYear:
+        `<li>
+    <b>Ano de conclusão:</b>
+    <textarea class="edit-lists" maxlength="4" placeholder="2020" spellcheck="false" title="Digite o ano de conclusão do seu curso" style="width: 150px;"></textarea>
+</li>`,
 
-    if (inProgress.checked && online.checked) {
-        alert("C")
-    }
+    websiteLink:
+        `<li>
+    <b>Link do Site:</b>
+    <textarea class="edit-lists" maxlength="26" placeholder="www.site.com" spellcheck="false" title="Digite o link do site de onde você fez seu curso" style="width: 185px;"></textarea>
+</li>`,
 
-    if (inProgress.checked && presential.checked) {
-        alert("D")
-    }
+    workload:
+        `<li>
+    <b>Carga horária:</b>
+    <textarea class="edit-lists" maxlength="10" placeholder="40 horas" spellcheck="false" title="Digite a carga horária do seu curso" style="width: 175px;"></textarea>
+</li>`,
+
+    local:
+        `<li>
+    <b>Local:</b>
+    <textarea class="edit-lists" maxlength="31" placeholder="Colégio Estadual Exemplo" spellcheck="false" title="Digite o local onde você fez o seu curso" style="width: 278px;"></textarea>
+</li>`,
+
+    predictedCompletion: `<li>
+    <b>Conclusão prevista:</b>
+    <textarea class="edit-lists" maxlength="4" placeholder="2024" spellcheck="false" title="Digite em que ano o seu curso irá ser finalizado" style="width: 140px;"></textarea>
+</li>`,
+
+    shift: `<li>
+    <b>Turno:</b>
+    <textarea class="edit-lists" maxlength="5" placeholder="Manhã" spellcheck="false" title="Digite o turno do seu curso" style="width: 276px;"></textarea>
+</li>`
 }
 
-function createSkillsArea() {
+// Verifica as opções selecionadas e chama uma função para montar o HTML.
+function checkOptions() {
+    let skillsArea;
 
+    if (academicEducation.checked)
+        skillsArea = academicEducationArea;
+
+    if (professionalSkills.checked)
+        skillsArea = professionalSkillsArea;
+
+    if (concluded.checked && online.checked) createSkillsArea(infoSkillsArea.conclusionYear, infoSkillsArea.websiteLink, infoSkillsArea.workload, skillsArea);
+    if (concluded.checked && presential.checked) createSkillsArea(infoSkillsArea.local, infoSkillsArea.conclusionYear, "", skillsArea);
+    if (inProgress.checked && online.checked) createSkillsArea(infoSkillsArea.predictedCompletion, infoSkillsArea.websiteLink, "", skillsArea);
+    if (inProgress.checked && presential.checked) createSkillsArea(infoSkillsArea.local, infoSkillsArea.shift, infoSkillsArea.predictedCompletion, skillsArea);
+}
+
+// Monta o HTML de acordo com os parâmetros passados.
+function createSkillsArea(skillA, skillB, skillC, skillArea) {
+    skillArea.innerHTML +=
+        `<div>
+        ${infoSkillsArea.institutionName}
+        ${infoSkillsArea.courseName}
+    </div>`;
+
+    skillArea.innerHTML +=
+        `<ul>
+        ${skillA}
+        ${skillB}
+        ${skillC}
+    </ul>`;
 }
